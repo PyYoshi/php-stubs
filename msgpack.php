890c7cb906331044bc77fe2d6bf7989859fe609f
PyYoshi <myoshi321go@gmail.com>
2014-05-12 16:27:04 +0900

    add msgpack stub

---
+++ msgpack.php
@@ -0,0 +1,15 @@
+<?php
+// msgpack-php 0.5.5
+// https://github.com/msgpack/msgpack-php
+
+/**
+ * @param string $msg
+ * @return mixed
+ */
+function msgpack_pack($msg){}
+
+/**
+ * @param mixed $data
+ * @return string
+ */
+function msgpack_unpack($data){}
